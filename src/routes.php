6fdf33a09701a056dc3a49c7ba0bbf618561194a
Reza Adibi <adbrezaooo@gmail.com>
2022-03-22 12:48:15 +0430

    updated dependencies

--- src/routes.php
+++ src/routes.php
@@ -1,4 +1,7 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
 Route::group(['namespace'=>'Ipecompany\Smsirlaravel\Controllers','middleware'=>config('smsirlaravel.middlewares')], function (){
 	Route::get(config('smsirlaravel.route'),'SmsirlaravelController@index')->name('sms-admin');
 	Route::get('sms-admin/{log}/delete','SmsirlaravelController@delete')->name('deleteLog');
